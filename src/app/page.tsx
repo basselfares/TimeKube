bf00ad3b163ef3cdcdcf59715823f3fbe56f73c8
Bassel Fares <basselmfares@gmail.com>
2023-12-09 03:15:39 -0500

    sidebar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image'
+import Sidebar from './sidebar/sidebar'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
-    </main>
+    <Sidebar/>
   )
 }
